feb49491bcaf358b1d7f32619ff0a4a3010387a9
ezz-mohamed <55768766+ezz-mohamed@users.noreply.github.com>
2021-04-30 18:31:45 +0200

    Add files via upload

---
+++ MainActivity(googlemap).java
@@ -0,0 +1,47 @@
+package com.ezz.googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.IMapFragmentDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+GoogleMap map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+      map = googleMap;
+
+        LatLng ElMahalla = new LatLng(30.970051996405296, 31.16831800864464);
+        map.addMarker(new MarkerOptions().position(ElMahalla).title("ElMahalla"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(ElMahalla));
+
+
+
+
+
+
+
+    }
+}
